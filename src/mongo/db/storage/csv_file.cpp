3a808af5b1e6a828e318be621f999e3d60f42411
June Kim <powerball701@gmail.com>
2024-04-05 15:45:40 -0700

    Add a CSV reader to implement `InputStream` Fixes codeincorp/mongo#4

---
+++ src/mongo/db/storage/csv_file.cpp
@@ -0,0 +1,348 @@
+/**
+ *    Copyright (C) 2024-present Codein, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+#include <cassert>
+#include <cerrno>
+#include <chrono>
+#include <cstdio>
+#include <fmt/format.h>
+#include <fstream>  // IWYU pragma: keep
+#include <string>
+#include <sys/stat.h>
+
+#include "mongo/base/error_codes.h"
+#include "mongo/base/string_data.h"
+#include "mongo/db/query/query_knobs_gen.h"
+#include "mongo/db/storage/csv_file.h"
+#include "mongo/db/storage/io_error_message.h"
+#include "mongo/util/assert_util.h"
+#include "mongo/util/time_support.h"
+
+namespace mongo {
+using namespace fmt::literals;
+
+CsvFileInput::CsvFileInput(const std::string& fileRelativePath, const std::string& metaFileRelative)
+    : _fileAbsolutePath((externalPipeDir == "" ? kDefaultPipePath : externalPipeDir) +
+                        fileRelativePath),
+      _metadataAbsolutePath((externalPipeDir == "" ? kDefaultPipePath : externalPipeDir) +
+                            metaFileRelative),
+      _ifs() {
+    uassert(200000400,  // (2,000,000 + issueid) * 100
+            "File path must not include '..' but {} does"_format(_fileAbsolutePath),
+            _fileAbsolutePath.find("..") == std::string::npos);
+}
+
+CsvFileInput::~CsvFileInput() {
+    _ifs.close();
+}
+
+void CsvFileInput::doOpen() {
+    std::string metadata;
+    _ifs.open(_metadataAbsolutePath.c_str(), std::ios::binary | std::ios::in);
+    uassert(ErrorCodes::FileNotOpen,
+            "File has failed to open. Make sure the file exists: "_format(
+                getErrorMessage("open", _metadataAbsolutePath)),
+            _ifs.is_open());
+    std::getline(_ifs, metadata);
+
+    _md = getMetadata(parseLine(metadata));
+    _ifs.close();
+
+    _ifs.open(_fileAbsolutePath.c_str(), std::ios::binary | std::ios::in);
+
+    uassert(ErrorCodes::FileNotOpen,
+            "error = {}"_format(getErrorMessage("open", _fileAbsolutePath)),
+            _ifs.is_open());
+}
+
+// caller must ensure that buffer size is greater than or equal to the size of the bsonObject
+// to be returned. If not, it would throw exception (Not enough size in buffer)
+int CsvFileInput::doRead(char* data, int size) {
+
+    auto bsonObj = readBsonObj();
+
+    if (!bsonObj) {
+        return 0;
+    }
+
+    int nRead = bsonObj->objsize();
+    uassert(200000402, "Buff Size too small", nRead <= size);
+    memcpy(data, bsonObj->objdata(), nRead);
+    return nRead;
+}
+
+void CsvFileInput::doClose() {
+    _ifs.close();
+}
+
+bool CsvFileInput::isOpen() const {
+    return _ifs.is_open();
+}
+
+bool CsvFileInput::isGood() const {
+    return _ifs.good();
+}
+
+bool CsvFileInput::isFailed() const {
+    return _ifs.fail();
+}
+
+bool CsvFileInput::isEof() const {
+    return _ifs.eof();
+}
+
+// assuming that header is returned by parseLine, which means, each of its element is
+// name of the field with type name
+Metadata CsvFileInput::getMetadata(const std::vector<std::string>& header) {
+    Metadata ret;
+
+    for (auto& field : header) {
+        int index = field.find('/');
+        int len = field.length() - index - 1;
+
+        auto fieldName = field.substr(0, index);
+        auto typeName = field.substr(index + 1, len);
+        CsvFieldType type;
+
+        if (typeName == "int" || typeName == "int32") {
+            type = CsvFieldType::kInt32;
+        } else if (typeName == "int64" || typeName == "long") {
+            type = CsvFieldType::kInt64;
+        } else if (typeName == "double") {
+            type = CsvFieldType::kDecimal;
+        } else if (typeName == "bool") {
+            type = CsvFieldType::kBool;
+        } else if (typeName == "oid") {
+            type = CsvFieldType::kOid;
+        } else if (typeName == "date") {
+            type = CsvFieldType::kDate;
+        } else if (typeName == "string") {
+            type = CsvFieldType::kString;
+        } else {
+            uasserted(200000401, typeName + " type specified in metadata is not supported");
+        }
+
+        ret.push_back({std::move(fieldName), type});
+    }
+
+    return ret;
+}
+
+template <CsvFieldType T>
+void appendTo(BSONObjBuilder& builder, const std::string& fieldName, const std::string& data);
+
+template <>
+void appendTo<CsvFieldType::kInt32>(BSONObjBuilder& builder,
+                                    const std::string& fieldName,
+                                    const std::string& data) {
+    builder.append(fieldName, stoi(data));
+    if (data.find('.') != std::string::npos) {
+        std::cout << std::endl
+                  << "Lossy conversion of decimal type to whole number type:   Line Number: Field: "
+                  << fieldName << std::endl;
+    }
+}
+
+template <>
+void appendTo<CsvFieldType::kDecimal>(BSONObjBuilder& builder,
+                                      const std::string& fieldName,
+                                      const std::string& data) {
+    builder.append(fieldName, stod(data));
+    int dotIndex = data.find('.');
+    // Warning user of possibly unwanted lossy conversion if provided decimal is too
+    // long
+    if (data.substr(dotIndex + 1, data.length() - dotIndex - 1).length() > 15) {
+        std::cout << std::endl
+                  << "Possible lossy conversion: " << data << " Line Number:  Field: " << std::endl;
+    }
+}
+
+template <>
+void appendTo<CsvFieldType::kInt64>(BSONObjBuilder& builder,
+                                    const std::string& fieldName,
+                                    const std::string& data) {
+    builder.append(fieldName, stoll(data));
+    if (data.find('.') != std::string::npos) {
+        std::cout << std::endl
+                  << "Lossy conversion of decimal type to whole number type:   Line Number: Field: "
+                  << fieldName << std::endl;
+    }
+}
+
+template <>
+void appendTo<CsvFieldType::kString>(BSONObjBuilder& builder,
+                                     const std::string& fieldName,
+                                     const std::string& data) {
+    builder.append(fieldName, data);
+}
+
+template <>
+void appendTo<CsvFieldType::kDate>(BSONObjBuilder& builder,
+                                   const std::string& fieldName,
+                                   const std::string& data) {
+    // what if the field only does not specify time? only date
+    auto date = dateFromISOString(data);
+    // return badValue error codes
+    uassert(date.getStatus().code(), date.getStatus().toString(), date.isOK());
+    builder.appendDate(fieldName, date.getValue());
+}
+
+template <>
+void appendTo<CsvFieldType::kOid>(BSONObjBuilder& builder,
+                                  const std::string& fieldName,
+                                  const std::string& data) {
+    constexpr size_t kLengthOidValue = 24;
+    constexpr size_t kOidTypeStr = 8;                      // objectid
+    constexpr size_t kOidTypeStrPrefix = kOidTypeStr + 2;  // objectid("
+
+    std::string mutableData = data;
+    // oid format should be either objectId("ID") or "ID", or just ID?
+    // what if there is remaining incorrect data after the substring?
+    if (data[0] == 'O' || data[0] == 'o') {
+        std::transform(
+            mutableData.begin(), mutableData.begin() + kOidTypeStr, mutableData.begin(), ::tolower);
+        uassert(ErrorCodes::BadValue,
+                "Invalid Object Id format",
+                mutableData.substr(0, 10) == "objectid(\"");
+        mutableData = mutableData.substr(kOidTypeStrPrefix, kLengthOidValue);
+    } else if (data[0] == '\"') {
+        mutableData = mutableData.substr(1, kLengthOidValue);
+    }
+
+    uassert(
+        ErrorCodes::BadValue, "Invalid Object Id Format", mutableData.length() == kLengthOidValue);
+    mongo::OID id = mongo::OID(mutableData);  // throws exception if bad
+    builder.append(fieldName, id);
+}
+
+template <>
+void appendTo<CsvFieldType::kBool>(BSONObjBuilder& builder,
+                                   const std::string& fieldName,
+                                   const std::string& data) {
+    std::string mutableData = data;
+    bool val;
+    std::transform(data.begin(), data.end(), mutableData.begin(), ::tolower);
+    if (mutableData == "true" || mutableData == "t" || mutableData == "yes" || mutableData == "y" ||
+        mutableData == "1")
+        val = true;
+    else if (mutableData == "false" || mutableData == "f" || mutableData == "no" ||
+             mutableData == "n" || mutableData == "0")
+        val = false;
+    else
+        uasserted(ErrorCodes::BadValue, "Invalid Boolean representation");
+    builder.append(fieldName, val);
+}
+
+
+boost::optional<BSONObj> CsvFileInput::readBsonObj() {
+    std::string record;
+    std::getline(_ifs, record);
+    // if eof is reached or _ifs return false in the first place, _ifs would not extract any byte
+    // from the file anyway. std::getline does not affect the istream::gcount, thus when appended
+    // to this conditional statement, may cause unexpected exit.
+    if (_ifs.eof() || _ifs.fail()) {
+        return boost::none;
+    }
+
+    auto data = parseLine(record);
+    uassert(ErrorCodes::TypeMismatch,
+            "Metadata and current record mismatch",
+            data.size() == _md.size());
+
+    BSONObjBuilder builder;
+
+    // what happens when csv field is missing? (data1,,data3)
+    // for the theme of schemaless database, it should be allowed
+    // currently stox would return exception, string would go on as usual, char return error,
+    // bool appens false, date would return bad date value error. OId I don't know
+    for (size_t i = 0; i < _md.size(); ++i) {
+        if (data[i] == "") {
+            builder.appendNull(_md[i].fieldName);
+            continue;
+        }
+
+        try {
+            switch (_md[i].fieldType) {
+                case CsvFieldType::kInt32:
+                    appendTo<CsvFieldType::kInt32>(builder, _md[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kDecimal:
+                    appendTo<CsvFieldType::kDecimal>(builder, _md[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kInt64:
+                    appendTo<CsvFieldType::kInt64>(builder, _md[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kString:
+                    appendTo<CsvFieldType::kString>(builder, _md[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kBool:
+                    appendTo<CsvFieldType::kBool>(builder, _md[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kOid:
+                    appendTo<CsvFieldType::kOid>(builder, _md[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kDate:
+                    appendTo<CsvFieldType::kDate>(builder, _md[i].fieldName, data[i]);
+                    break;
+            }
+            // catching failed conversion to stoi, stof, stoll, and stod
+        } catch (const std::invalid_argument& e) {
+            uasserted(ErrorCodes::BadValue, e.what());
+        } catch (const std::out_of_range& e) {
+            uasserted(ErrorCodes::Overflow, e.what());
+        }
+    }
+    return builder.done().getOwned();
+}
+
+std::vector<std::string> CsvFileInput::parseLine(const std::string& line) {
+    int left = 0;
+    int right = 0;
+    int len = line.length();
+    int i = 0;
+    std::vector<std::string> strs;
+
+    while (i <= len) {
+        if (i == len || line.at(i) == ',') {
+            // can it handle empty string?
+            strs.emplace_back(line.substr(left, right - left));
+            left = 0;
+            right = 0;
+        } else if (line.at(i) != ' ' && line.at(i) != '\t') {
+            if (right == 0) {
+                left = i;
+            }
+            right = i + 1;
+        }
+
+        i++;
+    }
+    return strs;
+}
+
+}  // namespace mongo
---
+++ src/mongo/db/storage/csv_file_test.cpp
@@ -0,0 +1,403 @@
+#include <vector>
+
+#include "csv_file.h"
+#include "mongo/bson/json.h"
+#include "mongo/unittest/assert.h"
+
+namespace mongo {
+using namespace fmt::literals;
+
+class CsvFileInputTest : public unittest::Test {};
+
+TEST_F(CsvFileInputTest, CsvBasicRead) {
+    CsvFileInput input("mongo_csv_test/basicReadData.csv", "mongo_csv_test/basicReadHeader.txt");
+
+    std::vector<mongo::BSONObj> expected = {fromjson(R"(
+{
+    field1: 12,
+    boolean: true,
+    decimal: 3.12345678901234522,
+    textField: "string",
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"),
+    moment: {$date: "2013-07-23T11:42:14.072Z"},
+    billionaire: 150000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 13, 
+    boolean: true, 
+    decimal: 1.2, 
+    textField: "string2", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment:{$date:"2017-08-06T13:13:59.010+07:00"},
+    billionaire: 120000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 14, 
+    boolean: false, 
+    decimal: 5.5, 
+    textField: "string3", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment: {$date:"2019-10-23T21:42:14.144Z"},
+    billionaire: 135000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 16, 
+    boolean: true, 
+    decimal: 6.6, 
+    textField: "string4", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment: {$date:"2016-11-11T01:16:23.543Z"},
+    billionaire: 2000000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 21, 
+    boolean: false, 
+    decimal: 0.9, 
+    textField: "string5", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment:{$date:"2017-10-31T08:23:49.982Z"},
+    billionaire: 100000000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 15, 
+    boolean: true, 
+    decimal: 3.3, 
+    textField: "string6", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment: {$date:"2012-12-23T23:59:54.932Z"},
+    billionaire: 53000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 18, 
+    boolean: true, 
+    decimal: 9.1, 
+    textField: "string7", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment:{$date:"1999-04-25T09:37:09.883Z"},
+    billionaire: 9000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 21, 
+    boolean: false, 
+    decimal: 3.98, 
+    textField: "string8", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment:{$date:"1970-03-28T07:34:42.390Z"},
+    billionaire: 70000000000
+})"),
+                                            fromjson(R"(
+{ 
+    field1: 31, 
+    boolean: true, 
+    decimal: 2.09, 
+    textField: "string9", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment: {$date:"2024-03-23T21:21:55.559Z"},
+    billionaire: 77000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 27, 
+    boolean: false, 
+    decimal: 12.34, 
+    textField: "string10", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment:{$date:"2023-12-30T12:12:14.645Z"},
+    billionaire: 77000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 41, 
+    boolean: false, 
+    decimal: 91.2, 
+    textField: "string11", 
+    docIdentifier: ObjectId("66075df233ce5deb424257fb"), 
+    moment:{$date:"2020-08-23T13:17:39.345Z"},
+    billionaire: 77000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 52, 
+    boolean: false, 
+    decimal: 93.2, 
+    textField: "string12", 
+    docIdentifier: ObjectId("660a04700ea7913a8fced3f4"), 
+    moment: {$date:"2013-07-03T03:23:23.900+05:00"},
+    billionaire: 77000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 91, 
+    boolean: true, 
+    decimal: 0.24, 
+    textField: "string13", 
+    docIdentifier: ObjectId("660a048f0ea7913a8fced3f6"), 
+    moment: {$date:"2006-12-12T12:38:48.985-04:00"},
+    billionaire: 77000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 123, 
+    boolean: true, 
+    decimal: 0.111, 
+    textField: "string14", 
+    docIdentifier: ObjectId("660a04910ea7913a8fced3f8"), 
+    moment: {$date:"2004-04-04T19:07:21.388-02:00"},
+    billionaire: 77000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 912, 
+    boolean: true, 
+    decimal: 1.231, 
+    textField: "string15", 
+    docIdentifier: ObjectId("660a04920ea7913a8fced3fa"), 
+    moment: {$date:"2010-11-11T21:21:59.991Z"},
+    billionaire: 77000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 1023, 
+    boolean: false, 
+    decimal: 5.121, 
+    textField: "string16", 
+    docIdentifier: ObjectId("660a04930ea7913a8fced3fc"), 
+    moment: {$date:"2011-09-09T13:29:31.211-06:00"},
+    billionaire: 77000000000
+})"),
+                                            fromjson(R"(
+{
+    field1: 34, 
+    boolean: true, 
+    decimal: .123, 
+    textField: "string17", 
+    docIdentifier: ObjectId("660a04940ea7913a8fced3fe"), 
+    moment: {$date:"2018-01-30T23:00:01.009Z"},
+    billionaire: 77000000000
+})")};
+
+    input.open();
+    ASSERT_TRUE(input.isOpen());
+
+    int bufSize = 200;
+    char buf[bufSize];
+    int nRead = 0;
+    int line = 0;
+    do {
+        ASSERT(input.isGood());
+        ASSERT(!input.isEof());
+        ASSERT(!input.isFailed());
+        nRead = input.read(buf, bufSize);
+
+        BSONObj obj(buf);
+        if (nRead > 0) {
+            ASSERT_BSONOBJ_EQ(obj, expected[line]);
+        }
+        line++;
+    } while (nRead != 0);
+
+    input.close();
+    ASSERT(!input.isOpen());
+}
+
+TEST_F(CsvFileInputTest, AbsentField) {
+    // StatusWith<Date_t> date = dateFromISOString("2017/04/25");
+    // std::cout << date.getStatus().toString() << std::endl;
+    std::vector<BSONObj> expected = {fromjson(R"({
+            things: "HedgeFund",
+            when: {$date: "2020-10-10T10:10:10.101-06:00"}, 
+            count: null,
+            long: 9000000000, 
+            identifier: ObjectId("123456789a123456789b1fcb"),
+            correct: true
+        })"),
+                                     fromjson(R"({
+            things: null,
+            when: {$date: "2020-10-10T10:10:10.101-06:00"},
+            count: 56,
+            long: 2300000000, 
+            identifier: ObjectId("a987654321bdbbcbeebfb528"),
+            correct: true
+        })"),
+                                     fromjson(R"({
+            things: "Schema", 
+            when: {$date: "2020-10-10T10:10:10.101-06:00"},
+            count: null,
+            long: 4000000000,
+            identifier: null,
+            correct: false
+        })"),
+                                     fromjson(R"({
+            things: "field1",
+            when: null,
+            count: 45,
+            long: null, 
+            identifier: null,
+            correct: true
+        })"),
+                                     fromjson(R"({
+            things: "field3",
+            when: {$date: "2020-10-10T10:10:10.101-06:00"},
+            count: 46,
+            long: 5000000000,
+            identifier: ObjectId("1234567890abcdefabcd12ef"),
+            correct: true
+        })"),
+                                     fromjson(R"({
+            things: "Texting", 
+            when: {$date: "2020-10-10T10:10:10.101-06:00"},
+            count: 47,
+            long: null, 
+            identifier: null,
+            correct: false
+        })"),
+                                     fromjson(R"({
+            things: "phone",
+            when: null,
+            count: 48,
+            long: 9876543210,
+            identifier: ObjectId("123456789012345678901234"),
+            correct: null
+        })"),
+                                     fromjson(R"({
+            things: "Hello World",
+            when: {$date: "2020-10-10T10:10:10.101-06:00"},
+            count: null,
+            long: 98765432123,
+            identifier: null,
+            correct: true
+        })"),
+                                     fromjson(R"({
+            things: "ipad",
+            when: {$date: "2020-10-10T10:10:10.101-06:00"},
+            count: 49,
+            long: null,
+            identifier: ObjectId("12345678901234567890aaaa"),
+            correct: false
+        })"),
+                                     fromjson(R"({
+            things: "remote", 
+            when: {$date: "2020-10-10T10:10:10.101-06:00"},
+            count: 50,
+            long: 77777777776,
+            identifier: null,
+            correct: null
+        })"),
+                                     fromjson(R"({
+            things: "controller", 
+            when: null,
+            count: null,
+            long: 44444444444,
+            identifier: null,
+            correct: false
+        })")};
+
+    std::cout << "error here" << std::endl;
+
+    CsvFileInput input("mongo_csv_test/absentField.csv", "mongo_csv_test/absentField.txt");
+    input.open();
+
+    int bufSize = 250;
+    char buf[bufSize];
+
+    for (int i = 0; i < 11; i++) {
+        input.read(buf, bufSize);
+        ASSERT_BSONOBJ_EQ(BSONObj(buf), expected[i]);
+    }
+
+    input.close();
+}
+
+// make separate test cases for bad long, bad int, bad string, bad bool, and so on?
+TEST_F(CsvFileInputTest, FailByBadValueOID) {
+    // ASSERT_THROWS(mongo::OID("This ain\'t 12 bytes"), DBException);
+    CsvFileInput input("mongo_csv_test/badOid.csv", "mongo_csv_test/badOid.txt");
+    input.open();
+
+    int bufSize = 100;
+    char buf[bufSize];
+
+    for (int i = 0; i < 9; i++) {
+        ASSERT_THROWS(input.read(buf, bufSize), DBException);
+    }
+
+    input.close();
+}
+
+TEST_F(CsvFileInputTest, FailByBadInt) {}
+
+TEST_F(CsvFileInputTest, FailByBadDate) {}
+
+TEST_F(CsvFileInputTest, FailByBadLong) {}
+
+TEST_F(CsvFileInputTest, FailByBadBoolean) {}
+
+TEST_F(CsvFileInputTest, FailByBadDecimal) {}
+
+TEST_F(CsvFileInputTest, FailByOverflow) {
+    CsvFileInput intOverflow("mongo_csv_test/intOverflow.csv", "mongo_csv_test/intOverflow.txt");
+    intOverflow.open();
+    int bufSize = 100;
+    char buf[bufSize];
+
+    for (int i = 0; i < 3; i++) {
+        ASSERT_THROWS_CODE(intOverflow.read(buf, bufSize), DBException, ErrorCodes::Overflow);
+    }
+    intOverflow.close();
+
+    CsvFileInput longOverflow("mongo_csv_test/longOverflow.csv", "mongo_csv_test/longOverflow.txt");
+    longOverflow.open();
+
+    for (int i = 0; i < 6; i++) {
+        ASSERT_THROWS_CODE(longOverflow.read(buf, bufSize), DBException, ErrorCodes::Overflow);
+    }
+}
+
+// underflow and overflow generate same type of exception: out_of_range
+TEST_F(CsvFileInputTest, FailByUnderflow) {}
+
+TEST_F(CsvFileInputTest, BufferTooSmall) {
+    // read should throw exception when buffer size is too small
+    CsvFileInput input("mongo_csv_test/bufTooSmall.csv", "mongo_csv_test/bufTooSmall.txt");
+    input.open();
+
+    int bufSize = 40;
+    char buf[bufSize];
+
+    for (int i = 0; i < 4; i++) {
+        ASSERT_THROWS(input.read(buf, bufSize), DBException);
+        // ASSERT_THROWS_CODE(input.read(buf, bufSize), DBException, 200000402);
+    }
+
+    input.close();
+}
+
+TEST_F(CsvFileInputTest, FailByFileDoesNotExist) {
+    CsvFileInput input("DNE.csv", "DNE.txt");
+    ASSERT_THROWS_CODE(input.open(), DBException, ErrorCodes::FileNotOpen);
+
+    CsvFileInput input1("DNE1.csv", "DNE1.txt");
+    ASSERT_THROWS_CODE(input.open(), DBException, ErrorCodes::FileNotOpen);
+
+    CsvFileInput input2("DNE2.csv", "mongo_csv_test/badOid.txt");
+    ASSERT_THROWS_CODE(input.open(), DBException, ErrorCodes::FileNotOpen);
+}
+
+TEST_F(CsvFileInputTest, FailByMetadataLengthMisMatch) {
+    CsvFileInput input("mongo_csv_test/diffLength.csv", "mongo_csv_test/diffLength.txt");
+    input.open();
+    char buff[200];
+    for (int i = 0; i < 3; i++) {
+        ASSERT_THROWS_CODE(input.read(buff, 200), DBException, ErrorCodes::TypeMismatch);
+    }
+
+    input.close();
+}
+}  // namespace mongo
