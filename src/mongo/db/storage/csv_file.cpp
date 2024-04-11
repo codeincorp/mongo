266bbd72691f8e8846a1d76a076b9f04529f54d9
June Kim <powerball701@gmail.com>
2024-04-11 15:27:47 -0700

    [codein] Add a CSV reader to implement `InputStream` (#7)

---
+++ src/mongo/db/storage/csv_file.cpp
@@ -0,0 +1,360 @@
+/**
+ *    Copyright (C) 2024-present Codein, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/db/storage/csv_file.h"
+
+#include <fmt/format.h>
+#include <fstream>  // IWYU pragma: keep
+#include <string>
+
+#include "mongo/base/error_codes.h"
+#include "mongo/bson/bsonobjbuilder.h"
+#include "mongo/bson/oid.h"
+#include "mongo/db/query/query_knobs_gen.h"
+#include "mongo/db/storage/io_error_message.h"
+#include "mongo/logv2/log.h"
+#include "mongo/util/assert_util.h"
+
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kCommand
+
+namespace mongo {
+
+using namespace fmt::literals;
+
+CsvFileInput::CsvFileInput(const std::string& fileRelativePath, const std::string& metaFileRelative)
+    : _fileAbsolutePath((externalPipeDir == "" ? kDefaultPipePath : externalPipeDir) +
+                        fileRelativePath),
+      _metadataAbsolutePath((externalPipeDir == "" ? kDefaultPipePath : externalPipeDir) +
+                            metaFileRelative),
+      _ifs() {
+    uassert(200000400,
+            "File path must not include '..' but {} does"_format(_fileAbsolutePath),
+            _fileAbsolutePath.find("..") == std::string::npos);
+    uassert(200000401,
+            "File path must not include '..' but {} does"_format(_metadataAbsolutePath),
+            _metadataAbsolutePath.find("..") == std::string::npos);
+}
+
+CsvFileInput::~CsvFileInput() {
+    _ifs.close();
+}
+
+void CsvFileInput::doOpen() {
+    std::string metadata;
+    std::ifstream ifsMetadata(_metadataAbsolutePath.c_str(), std::ios::in);
+    uassert(ErrorCodes::FileNotOpen,
+            "error = {}: "_format(getErrorMessage("open", _metadataAbsolutePath)),
+            ifsMetadata.is_open());
+    std::getline(ifsMetadata, metadata);
+
+    _metadata = getMetadata(parseLine(metadata));
+    ifsMetadata.close();
+
+    _ifs.open(_fileAbsolutePath.c_str(), std::ios::in);
+
+    uassert(ErrorCodes::FileNotOpen,
+            "error = {}"_format(getErrorMessage("open", _fileAbsolutePath)),
+            _ifs.is_open());
+}
+
+// Caller must ensure that buffer size is greater than or equal to the size of the bsonObject to be
+// returned. If not, it will throw an exception (Not enough size in buffer).
+int CsvFileInput::doRead(char* data, int size) {
+    auto bsonObj = readBsonObj();
+
+    if (!bsonObj) {
+        return 0;
+    }
+
+    int nRead = bsonObj->objsize();
+    uassert(200000402,
+            "Buff Size {} bytes is too small to contain {} bytes bsonObj"_format(size, nRead),
+            nRead <= size);
+    memcpy(data, bsonObj->objdata(), nRead);
+    return nRead;
+}
+
+void CsvFileInput::doClose() {
+    _ifs.close();
+}
+
+bool CsvFileInput::isOpen() const {
+    return _ifs.is_open();
+}
+
+bool CsvFileInput::isGood() const {
+    return _ifs.good();
+}
+
+bool CsvFileInput::isFailed() const {
+    return _ifs.fail();
+}
+
+bool CsvFileInput::isEof() const {
+    return _ifs.eof();
+}
+
+// Assuming that header is returned by parseLine, which means, each of its element contains
+// name of the field with its typeInfo as string. {"fieldName1/typeName1","fieldName/typeName1"...}
+Metadata CsvFileInput::getMetadata(const std::vector<std::string>& header) {
+    constexpr size_t typeNameAbsent = 0;
+    Metadata ret;
+
+    size_t fieldIndex = 0;
+    for (const auto& field : header) {
+        // Throws an exception when field does not contain '/' at all or typename is absent after
+        // '/'.
+        size_t separatorIndex = field.find('/');
+        size_t fieldLen = separatorIndex != std::string::npos ? field.length() - separatorIndex - 1
+                                                              : typeNameAbsent;
+        uassert(200000403,
+                "{}th Field '{}' does not specify typeName."_format(field, fieldIndex),
+                fieldLen > typeNameAbsent);
+
+        auto fieldName = field.substr(0, separatorIndex);
+        auto typeName = field.substr(separatorIndex + 1, fieldLen);
+        CsvFieldType fieldType;
+
+        if (typeName == "int" || typeName == "int32") {
+            fieldType = CsvFieldType::kInt32;
+        } else if (typeName == "int64" || typeName == "long") {
+            fieldType = CsvFieldType::kInt64;
+        } else if (typeName == "double") {
+            fieldType = CsvFieldType::kDouble;
+        } else if (typeName == "bool") {
+            fieldType = CsvFieldType::kBool;
+        } else if (typeName == "oid") {
+            fieldType = CsvFieldType::kOid;
+        } else if (typeName == "date") {
+            fieldType = CsvFieldType::kDate;
+        } else if (typeName == "string") {
+            fieldType = CsvFieldType::kString;
+        } else {
+            uasserted(200000404,
+                      "{} type is not supported at {}th field: {}"_format(
+                          typeName, fieldIndex, fieldName));
+        }
+
+        ret.push_back({std::move(fieldName), fieldType});
+        fieldIndex++;
+    }
+
+    return ret;
+}
+
+template <CsvFieldType T>
+void appendTo(BSONObjBuilder& builder, const std::string& fieldName, const std::string& data);
+
+template <>
+void appendTo<CsvFieldType::kInt32>(BSONObjBuilder& builder,
+                                    const std::string& fieldName,
+                                    const std::string& data) {
+    int converted = stoi(data);
+    builder.append(fieldName, stoi(data));
+    if (data.find('.') != std::string::npos) {
+        LOGV2_WARNING(
+            200000405,
+            "{field}: Lossy conversion of double type to int32 type: {original} To {converted}",
+            "field"_attr = fieldName,
+            "original"_attr = data,
+            "convertedTo"_attr = converted);
+    }
+}
+
+template <>
+void appendTo<CsvFieldType::kDouble>(BSONObjBuilder& builder,
+                                     const std::string& fieldName,
+                                     const std::string& data) {
+    builder.append(fieldName, stod(data));
+}
+
+template <>
+void appendTo<CsvFieldType::kInt64>(BSONObjBuilder& builder,
+                                    const std::string& fieldName,
+                                    const std::string& data) {
+    long long int converted = stoll(data);
+    builder.append(fieldName, converted);
+    if (data.find('.') != std::string::npos) {
+        LOGV2_WARNING(200000406,
+                      "{field}: lossy conversion of decimal type to int64 type: {original} "
+                      "converted to {convertedTo}",
+                      "field"_attr = fieldName,
+                      "original"_attr = data,
+                      "convertedTo"_attr = converted);
+    }
+}
+
+template <>
+void appendTo<CsvFieldType::kString>(BSONObjBuilder& builder,
+                                     const std::string& fieldName,
+                                     const std::string& data) {
+    builder.append(fieldName, data);
+}
+
+template <>
+void appendTo<CsvFieldType::kDate>(BSONObjBuilder& builder,
+                                   const std::string& fieldName,
+                                   const std::string& data) {
+    auto date = dateFromISOString(data);
+    uassert(date.getStatus().code(), date.getStatus().toString(), date.isOK());
+    builder.appendDate(fieldName, date.getValue());
+}
+
+template <>
+void appendTo<CsvFieldType::kOid>(BSONObjBuilder& builder,
+                                  const std::string& fieldName,
+                                  const std::string& data) {
+    constexpr size_t kLengthOidValue = 24;
+    constexpr size_t kOidTypeStr = 8;                      // Length of 'objectid'.
+    constexpr size_t kOidTypeStrPrefix = kOidTypeStr + 2;  // Length of 'objectid(\"'.
+
+    std::string mutableData = data;
+    if (data[0] == 'O' || data[0] == 'o') {
+        std::transform(
+            mutableData.begin(), mutableData.begin() + kOidTypeStr, mutableData.begin(), ::tolower);
+        uassert(ErrorCodes::BadValue,
+                "Invalid Object Id format: {}"_format(data),
+                mutableData.substr(0, kOidTypeStrPrefix) == "objectid(\"");
+        mutableData = mutableData.substr(kOidTypeStrPrefix, kLengthOidValue);
+    } else if (data[0] == '\"') {
+        mutableData = mutableData.substr(1, kLengthOidValue);
+    }
+
+    uassert(ErrorCodes::BadValue,
+            "Invalid Object Id Format: {}"_format(data),
+            mutableData.length() == kLengthOidValue);
+    mongo::OID id = mongo::OID(mutableData);  // Throws exception if it detects bad OID.
+    builder.append(fieldName, id);
+}
+
+template <>
+void appendTo<CsvFieldType::kBool>(BSONObjBuilder& builder,
+                                   const std::string& fieldName,
+                                   const std::string& data) {
+    std::string mutableData = data;
+    bool val;
+    std::transform(data.begin(), data.end(), mutableData.begin(), ::tolower);
+    if (mutableData == "true" || mutableData == "t" || mutableData == "yes" || mutableData == "y" ||
+        mutableData == "1")
+        val = true;
+    else if (mutableData == "false" || mutableData == "f" || mutableData == "no" ||
+             mutableData == "n" || mutableData == "0")
+        val = false;
+    else
+        uasserted(ErrorCodes::BadValue, "{} is an invalid Boolean representation"_format(data));
+    builder.append(fieldName, val);
+}
+
+
+boost::optional<BSONObj> CsvFileInput::readBsonObj() {
+    std::string record;
+    std::getline(_ifs, record);
+    // If eof is reached, _ifs return false.
+    if (_ifs.eof() || _ifs.fail()) {
+        return boost::none;
+    }
+
+    auto data = parseLine(record);
+    if (data.size() != _metadata.size()) {
+        LOGV2_WARNING(200000407,
+                      "Metadata specifies {n1} fields but current record has {n2}",
+                      "n1"_attr = _metadata.size(),
+                      "n2"_attr = data.size());
+    }
+
+    // Just process as many fields as possible
+    size_t processableFields = std::min(data.size(), _metadata.size());
+
+    BSONObjBuilder builder;
+
+    for (size_t i = 0; i < processableFields; ++i) {
+        if (data[i] == "") {
+            builder.appendNull(_metadata[i].fieldName);
+            continue;
+        }
+
+        try {
+            switch (_metadata[i].fieldType) {
+                case CsvFieldType::kInt32:
+                    appendTo<CsvFieldType::kInt32>(builder, _metadata[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kDouble:
+                    appendTo<CsvFieldType::kDouble>(builder, _metadata[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kInt64:
+                    appendTo<CsvFieldType::kInt64>(builder, _metadata[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kString:
+                    appendTo<CsvFieldType::kString>(builder, _metadata[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kBool:
+                    appendTo<CsvFieldType::kBool>(builder, _metadata[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kOid:
+                    appendTo<CsvFieldType::kOid>(builder, _metadata[i].fieldName, data[i]);
+                    break;
+                case CsvFieldType::kDate:
+                    appendTo<CsvFieldType::kDate>(builder, _metadata[i].fieldName, data[i]);
+                    break;
+            }
+            // Catching failed conversion to stoi, stoll, and stod.
+        } catch (const std::invalid_argument& e) {
+            uasserted(ErrorCodes::BadValue, "{}: {}"_format(e.what(), data[i]));
+        } catch (const std::out_of_range& e) {
+            uasserted(ErrorCodes::Overflow, "{}: {}"_format(e.what(), data[i]));
+        }
+    }
+    return builder.done().getOwned();
+}
+
+std::vector<std::string> CsvFileInput::parseLine(const std::string& line) {
+    int left = 0;
+    int right = 0;
+    int len = line.length();
+    int i = 0;
+    std::vector<std::string> strs;
+
+    while (i <= len) {
+        if (i == len || line.at(i) == ',') {
+            strs.emplace_back(line.substr(left, right - left));
+            left = 0;
+            right = 0;
+        } else if (line.at(i) != ' ' && line.at(i) != '\t') {
+            if (right == 0) {
+                left = i;
+            }
+            right = i + 1;
+        }
+
+        i++;
+    }
+    return strs;
+}
+
+}  // namespace mongo
---
+++ src/mongo/db/storage/csv_file.h
@@ -0,0 +1,102 @@
+/**
+ *    Copyright (C) 2024-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include <boost/optional.hpp>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "mongo/db/storage/input_object.h"
+
+namespace mongo {
+
+static constexpr auto kDefaultPipePath = "/tmp/"_sd;
+
+enum class CsvFieldType { kBool, kInt32, kInt64, kDate, kOid, kDouble, kString };
+
+struct FieldInfo {
+    std::string fieldName;
+    CsvFieldType fieldType;
+};
+
+using Metadata = std::vector<FieldInfo>;
+
+class CsvFileInput : public StreamableInput {
+public:
+    CsvFileInput(const std::string& fileRelativePath, const std::string& metadataRelativePath);
+    ~CsvFileInput() override;
+    const std::string& getAbsolutePath() const override {
+        return _fileAbsolutePath;
+    }
+    bool isOpen() const override;
+    bool isGood() const override;
+    bool isFailed() const override;
+    bool isEof() const override;
+
+protected:
+    void doOpen() override;
+    // Read the data into pre-allocated buffer 'data' upto 'size' bytes at maximum and returns the
+    // number of bytes that has been actually read.
+    int doRead(char* data, int size) override;
+    void doClose() override;
+
+private:
+    /**
+     * Gets metadata from the header read by parseLine. Metadata contains information on the name of
+     * the field and type of the field.
+     *
+     * @param header read from the metadata file in vector, in format {"fieldName/typeName",
+     *     "fieldName/typeName"....}.
+     * @return: vector of FieldInfo containing fieldName(as std::string) and typeInfo(as
+     *     CsvFieldType) of the said field. {{"fieldName1",type1},{"fieldName2",type2}...}.
+     */
+    Metadata getMetadata(const std::vector<std::string>& header);
+
+    /**
+     * Reads each line read from csv file (specified by fileAbsolutePath) and parse it into each
+     * field as string.
+     *
+     * @param line read from csv, "data1,data2,data3..."
+     * @return: vector containing each field as string, {"data1","data2","data3"...}.
+     */
+    std::vector<std::string> parseLine(const std::string& line);
+
+    // Reads each line from the CSV file and converts it into a BSONObj, being compliant with the
+    // metadata. it will return boost::none if there is no more line to read in the csv file
+    boost::optional<BSONObj> readBsonObj();
+
+    std::string _fileAbsolutePath;
+    std::string _metadataAbsolutePath;
+    std::ifstream _ifs;
+    Metadata _metadata;
+};
+
+}  // namespace mongo
