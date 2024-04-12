d36d1251feab03e13eec1ccdbd9606d7ab3c9a90
Codein Dev <dev@codeinco.io>
2024-04-12 16:45:59 -0700

    Add more test cases

--- jstests/noPassthrough/virtual/vcoll_over_csv_file.js
+++ jstests/noPassthrough/virtual/vcoll_over_csv_file.js
@@ -1,5 +1,5 @@
 /**
- * Tests virtual collections over external CSV file.
+ * Tests virtual collections over external CSV file(s).
  *
  * @tags: [
  * # This test file requires multi-threading for writers and tends to fail on small machines due to
@@ -13,29 +13,27 @@ const conn = MongoRunner.runMongod();
 const db = conn.getDB(jsTestName());
 db.dropDatabase();
 
-const expected = [
-    {
-        "firstName": "Mary",
-        "lastName": "Miller",
-        "age": 10,
-        "subscriptionDate": ISODate("2023-01-23T00:00:00Z"),
-        "retired": false
-    },
-    {
-        "firstName": "John",
-        "lastName": "William",
-        "age": 65,
-        "subscriptionDate": ISODate("2013-07-15T00:00:00Z"),
-        "retired": true
-    },
-    {
-        "firstName": "James",
-        "lastName": "Robert",
-        "age": 90,
-        "subscriptionDate": ISODate("2024-03-01T00:00:00Z"),
-        "retired": true
-    }
-];
+const kMaryMiller = {
+    "firstName": "Mary",
+    "lastName": "Miller",
+    "age": 10,
+    "subscriptionDate": ISODate("2023-01-23T00:00:00Z"),
+    "retired": false
+};
+const kJohnWilliam = {
+    "firstName": "John",
+    "lastName": "William",
+    "age": 65,
+    "subscriptionDate": ISODate("2013-07-15T00:00:00Z"),
+    "retired": true
+};
+const kJamesRobert = {
+    "firstName": "James",
+    "lastName": "Robert",
+    "age": 90,
+    "subscriptionDate": ISODate("2024-03-01T00:00:00Z"),
+    "retired": true
+};
 
 removeFile("/tmp/test.csv");
 copyFile(pwd() + "/jstests/noPassthrough/virtual/test.csv", "/tmp/test.csv");
@@ -54,6 +52,7 @@ const coll = db.ext_csv;
         }
     });
 
+    const expected = [kMaryMiller, kJohnWilliam, kJamesRobert];
     const res = coll.find().toArray();
     assert.eq(res.length, expected.length, `Expected ${tojson(res)} but got ${tojson(expected)}`);
     assert.eq(res, expected, `Expected ${tojson(res)} but got ${tojson(expected)}`);
@@ -71,16 +70,126 @@ const coll = db.ext_csv;
             metadataUrl: "file://test.txt"
         }
     });
+
+    const allDocs = [kMaryMiller, kJohnWilliam, kJamesRobert];
+    const expected = allDocs.concat(allDocs);
     const res = coll.find().toArray();
-    const doubleExpected = expected.concat(expected);
-    assert.eq(res.length,
-              doubleExpected.length,
-              `Expected ${tojson(res)} but got ${tojson(doubleExpected)}`);
-    assert.eq(res, doubleExpected, `Expected ${tojson(res)} but got ${tojson(doubleExpected)}`);
+    assert.eq(res.length, expected.length, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+    assert.eq(res, expected, `Expected ${tojson(res)} but got ${tojson(expected)}`);
 })();
 
-(function testNonExistentFile() {
-    jsTestLog("Running testNonExistentFile()");
+(function testFilterSanity() {
+    jsTestLog("Running testFilterSanity()");
+    coll.drop();
+    db.createCollection("ext_csv", {
+        virtual: {
+            dataSources: [{url: "file://test.csv", storageType: "file", fileType: "csv"}],
+            metadataUrl: "file://test.txt"
+        }
+    });
+
+    const expected = [kJohnWilliam, kJamesRobert];
+    const res = coll.find({age: {$gt: 10}}).toArray();
+    assert.eq(res.length, expected.length, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+    assert.eq(res, expected, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+})();
+
+(function testGroupSanity1() {
+    jsTestLog("Running testGroupSanity1()");
+    coll.drop();
+    db.createCollection("ext_csv", {
+        virtual: {
+            dataSources: [{url: "file://test.csv", storageType: "file", fileType: "csv"}],
+            metadataUrl: "file://test.txt"
+        }
+    });
+
+    const expected = [{_id: false, c: 1}, {_id: true, c: 2}];
+    const res = coll.aggregate([{$group: {_id: "$retired", c: {$sum: 1}}}]).toArray();
+    assert.eq(res.length, expected.length, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+    assert.sameMembers(res, expected, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+})();
+
+(function testGroupSanity2() {
+    jsTestLog("Running testGroupSanity2()");
+    coll.drop();
+    db.createCollection("ext_csv", {
+        virtual: {
+            dataSources: [
+                {url: "file://test.csv", storageType: "file", fileType: "csv"},
+                {url: "file://test.csv", storageType: "file", fileType: "csv"},
+                {url: "file://test.csv", storageType: "file", fileType: "csv"}
+            ],
+            metadataUrl: "file://test.txt"
+        }
+    });
+
+    const expected =
+        [{_id: "Mary Miller", c: 3}, {_id: "John William", c: 3}, {_id: "James Robert", c: 3}];
+    const res =
+        coll.aggregate([{$group: {_id: {$concat: ["$firstName", " ", "$lastName"]}, c: {$sum: 1}}}])
+            .toArray();
+    assert.eq(res.length, expected.length, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+    assert.sameMembers(res, expected, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+})();
+
+(function testSortSanity() {
+    jsTestLog("Running testSortSanity()");
+    coll.drop();
+    db.createCollection("ext_csv", {
+        virtual: {
+            dataSources: [{url: "file://test.csv", storageType: "file", fileType: "csv"}],
+            metadataUrl: "file://test.txt"
+        }
+    });
+
+    const expected = [kJamesRobert, kMaryMiller, kJohnWilliam];
+    const res = coll.find({}, {}, {sort: {subscriptionDate: -1}}).toArray();
+    assert.eq(res.length, expected.length, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+    assert.sameMembers(res, expected, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+})();
+
+(function testSortLimitSanity() {
+    jsTestLog("Running testSortLimitSanity()");
+    coll.drop();
+    db.createCollection("ext_csv", {
+        virtual: {
+            dataSources: [{url: "file://test.csv", storageType: "file", fileType: "csv"}],
+            metadataUrl: "file://test.txt"
+        }
+    });
+
+    const expected = [kMaryMiller];
+    const res = coll.find({}, {}, {sort: {age: 1}}).limit(1).toArray();
+    assert.sameMembers(res, expected, `Expected ${tojson(res)} but got ${tojson(expected)}`);
+})();
+
+(function testUnsupportedFileType() {
+    jsTestLog("Running testUnsupportedFileType()");
+    coll.drop();
+    assert.commandFailedWithCode(db.createCollection("ext_csv", {
+        virtual: {
+            dataSources: [{url: "file://test.csv", storageType: "file", fileType: "yml"}],
+            metadataUrl: "file://test.txt"
+        }
+    }),
+                                 ErrorCodes.BadValue);
+})();
+
+(function testUnsupportedStorageType() {
+    jsTestLog("Running testUnsupportedStorageType()");
+    coll.drop();
+    assert.commandFailedWithCode(db.createCollection("ext_csv", {
+        virtual: {
+            dataSources: [{url: "file://test.csv", storageType: "block", fileType: "csv"}],
+            metadataUrl: "file://test.txt"
+        }
+    }),
+                                 ErrorCodes.BadValue);
+})();
+
+(function testNonExistentCsvFile() {
+    jsTestLog("Running testNonExistentCsvFile()");
     coll.drop();
     db.createCollection("ext_csv", {
         virtual: {
@@ -88,6 +197,22 @@ const coll = db.ext_csv;
             metadataUrl: "file://test.txt"
         }
     });
+
+    assert.throwsWithCode(() => {
+        coll.find().toArray();
+    }, ErrorCodes.FileNotOpen);
+})();
+
+(function testNonExistentMetadata() {
+    jsTestLog("Running testNonExistentMetadata()");
+    coll.drop();
+    db.createCollection("ext_csv", {
+        virtual: {
+            dataSources: [{url: "file://test.csv", storageType: "file", fileType: "csv"}],
+            metadataUrl: "file://test1.txt"
+        }
+    });
+
     assert.throwsWithCode(() => {
         coll.find().toArray();
     }, ErrorCodes.FileNotOpen);
